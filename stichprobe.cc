0b721f3cd61f425cb820b8b51efdc74b3cc7b88a
Michael Hufschmidt <michael@hufschmidt-web.de>
2020-11-18 09:46:27 +0100

    more simple approach for calculation variance

--- stichprobe.cc
+++ stichprobe.cc
@@ -5,26 +5,24 @@
 
 int main() {
   int n = 0, a = 0;
-  double a_sum = 0.0, mean = 0.0, a_square = 0.0, variance, sigma;
+  double a_sum = 0.0, a2_sum = 0.0, mean, variance, sigma;
  
   // 1. a)
   std::ifstream fin1("datensumme.txt");
   while (fin1 >> a) {
-	a_sum += a;
     n += 1;
+	a_sum += a;
+	a2_sum += a * a;
   };
   fin1.close();
   std::cout << "Sum over " << n << " values = " << a_sum << std::endl;
   mean = a_sum / n;
   std::cout << "Mean value <a> = " << mean << std::endl;
 
-  // 1. b) 
-  std::ifstream fin2("datensumme.txt");
-  while (fin2 >> a) {
-	a_square += (a - mean) * (a - mean);
-  }
-  fin2.close();
-  variance = a_square / n;
+  // 1. b)
+  // more simple approach using:
+  //   sum (ai - <a>)^2 = sum (ai)^2 - ( sum (ai) )^2 / n
+  variance = (a2_sum - a_sum * a_sum / n) / n;
   std::cout << "Variance = " << variance << std::endl;
 
   // 1. c) 
